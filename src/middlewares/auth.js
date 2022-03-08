0873dba2982c4e61d12d55b011f7a42d171995ac
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-03-08 14:42:23 +0100

    Handled bugs

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,13 +5,13 @@ const AppError = require("../errors/appError");
 
 exports.auth = async (req, res, next) => {
     const token = req.cookies.auth;
-    if (!token) next(new AppError("Token Not Found!", 404));
+    if (!token) return next(new AppError("Token Not Found!", 404));
 
     const user = await User.findOne({ token });
-    if (!user) next(new AppError("User Not Logged In!", 400));
+    if (!user) return next(new AppError("User Not Logged In!", 400));
 
     const decoded = jwt.decode(token);
-    if (!decoded) next(new AppError("User Authorization Failed", 403));
+    if (!decoded) return next(new AppError("User Authorization Failed", 403));
 
     req.USER_ID = user._id || undefined;
     next();
