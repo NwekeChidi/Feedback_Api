242e4549bcf8ddaf52e8a7effae4a1f91ca19557
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-03-15 14:28:59 +0100

    fixed timer in jwt

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 exports.sign = ( userName ) => {
-    return jwt.sign( { userName }, process.env.JWT_SECRET, {expiresIn: 60 * 60} );
+    return jwt.sign( { userName }, process.env.JWT_SECRET, {expiresIn: 60 * 3600} );
 }
 
 exports.decode = ( token ) => {
