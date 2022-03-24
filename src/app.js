53d2542318a1c12ee67c28b9a2c6b818ea39fdf8
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-03-24 17:02:48 +0100

    DEBUGGING

--- src/app.js
+++ src/app.js
@@ -18,8 +18,13 @@ app.use(express.static('public'));
 
 // enable cors for specific route
 app.use(cors({
-  origin: ['http://localhost','http://localhost:3000', 'http://localhost:3001', "http://localhost:3002" ]
+  origin: ['http://localhost', 'http://localhost:3000', 'http://localhost:3001', "http://localhost:3002"],
+  credentials: true
 }))
+app.use( (req, res, next) => {
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+})
 
 // enable morgan
 app.use(morgan('dev'));
