8c5008fed9aa88a1588db9268294ac67fb31d631
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-04-08 21:12:47 +0100

    chore: added extra header

--- src/app.js
+++ src/app.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 
 // enable cors for specific route
 app.use(cors({
-  origin: "https://shimmering-sprite-84ee41.netlify.app",
+  origin: ["https://shimmering-sprite-84ee41.netlify.app", "localhost:3000"],
   credentials: true
 }))
 app.use( (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://shimmering-sprite-84ee41.netlify.app');
+  res.header('Access-Control-Allow-Origin', ['https://shimmering-sprite-84ee41.netlify.app', 'localhost:3000']);
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   next()
